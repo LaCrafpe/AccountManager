21a14891e57cad56cb9dc899c2f4f8893468a5ac
CrazyMurray <lucasjob@job-lucas.local>
2018-08-03 17:03:56 +0200

    start routing

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucasjob
+ * Date: 03/08/2018
+ * Time: 16:11
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @return Response
+     * @throws \Exception
+     * @Route("/project/accounting")
+     */
+    public function index()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('index.html.twig', [
+            'number' => $number
+        ]);
+    }
+
+}
