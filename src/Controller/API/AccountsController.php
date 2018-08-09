9ee34264c479e7a0bb6df1972945de23d7ca8d55
Lucas Job <lucasjob@job-lucas.local>
2018-08-09 16:14:59 +0200

    adding table

--- src/Controller/API/AccountsController.php
+++ src/Controller/API/AccountsController.php
@@ -12,7 +12,6 @@ namespace App\Controller\API;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class AccountsController extends AbstractController
 {
@@ -54,17 +53,17 @@ class AccountsController extends AbstractController
             ],
             'depenses' => [
                 '700',
-                '600',
-                '550',
-                '334',
-                '1100',
-                '1300',
-                '1000',
-                '1200',
                 '1400',
-                '1500',
-                '1300',
-                '1800'
+                '1450',
+                '1184',
+                '1523',
+                '1484',
+                '1647',
+                '1362',
+                '1285',
+                '1584',
+                '1447',
+                '844'
             ],
             'revenus' => [
                 '1500',
@@ -80,6 +79,45 @@ class AccountsController extends AbstractController
                 '1247',
                 '1344'
             ],
+        ];
+
+        return $this->json($aData);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @Route("/project/accounting/table")
+     */
+    public function getStatement(Request $request)
+    {
+
+        $aData = [
+            'salaire' => [
+                'type' => 'income',
+                'yearly' => '20000',
+                'average' => '24000'
+            ],
+            'loyer' => [
+                'type' => 'outcome',
+                'yearly' => '-7200',
+                'average' => '-7200'
+            ],
+            'Internet' => [
+                'type' => 'outcome',
+                'yearly' => '-348',
+                'average' => '-348'
+            ],
+            'EDF' => [
+                'type' => 'outcome',
+                'yearly' => '-900',
+                'average' => '-864'
+            ],
+            'Total' => [
+                'type' => 'total',
+                'yearly' => '11552',
+                'average' => '15588'
+            ]
         ];
 
         return $this->json($aData);
